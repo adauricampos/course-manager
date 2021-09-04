f94c745296070a0388984b7c1c0d80d1c7bdfb09
adauricampos <adauridicampos@gmail.com>
2021-09-04 20:20:00 -0300

    tratando eventos data biding

---
+++ src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course {
+  imageUrl: string;
+  name: string;
+  id: number;
+  price: number;
+  code: string;
+  duration: number;
+  rating: number;
+  releaseDate: string;
+  description: string;
+}
